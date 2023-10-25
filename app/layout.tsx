4c6cfce70eba381ff9ad9e519bec6b29d71d9799
Alabere-coder <alabere4real247@gmail.com>
2023-10-25 09:51:46 +0100

    initial commits

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@radix-ui/themes/styles.css";
+import "./globals.css";
+import { Theme } from "@radix-ui/themes";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "./providers/modal-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "E-Empire",
+  description: "e-empire admin dashboards",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Theme>
+            <ModalProvider />
+            {children}
+          </Theme>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
